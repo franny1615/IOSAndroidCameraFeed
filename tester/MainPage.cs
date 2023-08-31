efb7d57e5401603609755c321a90baae0c248d69
frank <franciscofunes5@gmail.com>
2023-08-30 19:45:55 -0700

    pushing up the tester app

---
+++ tester/MainPage.cs
@@ -0,0 +1,20 @@
+﻿using Maui.NativeCamera;
+
+namespace NativeCameraTester;
+
+public class MainPage : ContentPage
+{
+	public MainPage()
+	{
+		Content = new Grid
+		{
+			Children =
+			{
+				new NativeCameraView
+				{
+					CameraPosition = CameraPosition.FrontFacing
+				}
+			}
+		};
+	}
+}
---
+++ tester/MauiProgram.cs
@@ -0,0 +1,22 @@
+﻿using Maui.NativeCamera;
+using Microsoft.Extensions.Logging;
+
+namespace NativeCameraTester;
+
+public static class MauiProgram
+{
+	public static MauiApp CreateMauiApp()
+	{
+		var builder = MauiApp.CreateBuilder();
+		builder
+			.UseMauiNativeCamera()
+			.UseMauiApp<App>();
+
+#if DEBUG
+		builder.Logging.AddDebug();
+#endif
+
+		return builder.Build();
+	}
+}
+
